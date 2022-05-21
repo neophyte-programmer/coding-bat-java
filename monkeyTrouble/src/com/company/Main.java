779b241b59fc0a71dcd3065ca3fa78a596428a92
Nutifafa Attor <attorfafa@gmail.com>
2022-05-21 17:35:20 +0000

    minor changes

---
+++ monkeyTrouble/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+// We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+// We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+//
+//
+//        monkeyTrouble(true, true) → true
+//        monkeyTrouble(false, false) → true
+//        monkeyTrouble(true, false) → false
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
