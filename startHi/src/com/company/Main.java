069f258da204d9f3350dafe252ee9c4240323703
Nutifafa Attor <attorfafa@gmail.com>
2022-05-21 14:48:18 +0000

    finished startHi

--- startHi/src/com/company/Main.java
+++ startHi/src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
-public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+//Given a string, return true if the string starts with "hi" and false otherwise.
+//
+//        startHi("hi there") → true
+//        startHi("hi") → true
+//        startHi("hello hi") → false
+
+public class Main {
+    static boolean startHi(String str) {
+        if (str.length() > 1){
+            String firstTwo = str.substring(0,2);
+            boolean isEqual = firstTwo.equals("hi");
+            if (isEqual) {
+                return true;
+            }
+        }
+        return false;
     }
+    public static void main(String[] args) {
+       boolean word = startHi("hello");
+       if (word) {
+           System.out.println("True");
+       }
+       else {
+           System.out.println("False");
+       }
+    }
+
+
 }
