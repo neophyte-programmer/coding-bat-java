abb0ea41549af640ec905eb77c2479cce8d494df
Nutifafa Attor <attorfafa@gmail.com>
2022-05-22 06:37:53 +0000

    in1020

--- in1020/src/com/company/Main.java
+++ in1020/src/com/company/Main.java
@@ -7,6 +7,13 @@ public class Main {
 //            in1020(21, 12) → true
 //            in1020(8, 99) → false
 
+    public boolean in1020(int a, int b) {
+        if (a < 21 && a > 9 || b < 21 && b > 9){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
