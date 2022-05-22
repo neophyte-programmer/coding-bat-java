de3552e3e85c10f4a77b36a46ff152b809c9764e
Nutifafa Attor <attorfafa@gmail.com>
2022-05-22 06:23:03 +0000

    icyHot

---
+++ in1020/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+//    Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+//            in1020(12, 99) → true
+//            in1020(21, 12) → true
+//            in1020(8, 99) → false
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
